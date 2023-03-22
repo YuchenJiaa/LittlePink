58e7f260787c053155dbd007c61b65a46cd39a31
YuchenJiaa <113528470+YuchenJiaa@users.noreply.github.com>
2023-03-22 19:17:18 -0400

    use picker variable to pick image

--- LittlePink/TabBarC.swift
+++ LittlePink/TabBarC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import YPImagePicker
 
 class TabBarC: UITabBarController, UITabBarControllerDelegate {
 
@@ -17,7 +18,17 @@ class TabBarC: UITabBarController, UITabBarControllerDelegate {
     //if return true, then will show a classic show, otherwise a customize show
     func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
         if let vc = viewController as? ShareVC{
-            
+
+            let picker = YPImagePicker()
+            picker.didFinishPicking { [unowned picker] items, _ in
+                if let photo = items.singlePhoto {
+                    print(photo.fromCamera) // Image source (camera or library)
+                    print(photo.image) // Final image selected by the user
+                    print(photo.originalImage) // original image selected by the user, unfiltered
+                }
+                picker.dismiss(animated: true, completion: nil)
+            }
+            present(picker, animated: true, completion: nil)
             
             return false
         }
