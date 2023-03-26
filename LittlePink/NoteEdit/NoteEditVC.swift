983c56df42ef316e0f09ae4543cf09a8aa391a7c
YuchenJiaa <113528470+YuchenJiaa@users.noreply.github.com>
2023-03-26 18:25:40 -0400

    Add UITextField, editing did begin and editing did end action

--- LittlePink/NoteEdit/NoteEditVC.swift
+++ LittlePink/NoteEdit/NoteEditVC.swift
@@ -6,9 +6,7 @@
 //
 
 import UIKit
-import YPImagePicker
-import SKPhotoBrowser
-import AVKit
+
 
 class NoteEditVC: UIViewController {
 
@@ -18,8 +16,13 @@ class NoteEditVC: UIViewController {
     
     //var videoURL: URL = Bundle.main.url(forResource: "testVideo", withExtension: "mp4")!
     var videoURL: URL?
+    
     @IBOutlet weak var photoCollectionView: UICollectionView!
-
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var titleCountLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
+    
     var photoCount: Int{ photos.count }
     var isVideo: Bool{videoURL != nil}
     
@@ -30,109 +33,15 @@ class NoteEditVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-}
-
-extension NoteEditVC: UICollectionViewDataSource{
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        photoCount
-    }
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kPhotoCellID, for: indexPath) as! PhotoCell
-        
-        cell.imageView.image = photos[indexPath.item]
-        
-        return cell
+    @IBAction func TFEditBegin(_ sender: Any) {
+        titleCountLabel.isHidden = false
     }
     
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        
-        switch kind {
-        case UICollectionView.elementKindSectionFooter:
-            
-            let photoFooter = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: kPhotoFooterID, for: indexPath) as! PhotoFooter
-            photoFooter.addPhotoButton.addTarget(self, action: #selector(addPhoto), for: .touchUpInside)
-            return photoFooter
-        default:
-            fatalError("The footer of collectionView goes wrong.")
-        }
-    }
-}
-
-//MARK: - UICollectionViewDelegate
-extension NoteEditVC: UICollectionViewDelegate{
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        //Preview video or photo.
-        if isVideo{
-            let playerVC = AVPlayerViewController()
-            playerVC.player = AVPlayer(url: videoURL!)
-            present(playerVC, animated: true){
-                playerVC.player?.play()
-            }
-        }else{
-            // 1. create SKPhoto Array from UIImage
-            var images: [SKPhoto] = []
-            
-            for photo in photos{
-                images.append(SKPhoto.photoWithImage(photo))
-            }
-            // 2. create PhotoBrowser Instance, and present from your viewController.
-            let browser = SKPhotoBrowser(photos: images, initialPageIndex: indexPath.item)
-            browser.delegate = self
-            SKPhotoBrowserOptions.displayAction = false
-            SKPhotoBrowserOptions.displayDeleteButton = true
-            present(browser, animated: true)
-
-        }
-        
+    
+    @IBAction func TFEditEnd(_ sender: Any) {
+        titleCountLabel.isHidden = true
     }
     
-}
-//MARK: - SKPhotoBrowserDelegate
-extension NoteEditVC: SKPhotoBrowserDelegate{
-    func removePhoto(_ browser: SKPhotoBrowser, index: Int, reload: @escaping (() -> Void)) {
-        photos.remove(at: index)
-        photoCollectionView.reloadData()
-        reload()
-    }
-}
-//MARK: - Listening
-extension NoteEditVC{
-    @objc private func addPhoto(){
-        if photoCount < kMaxPhotoCount{
-            var config = YPImagePickerConfiguration()
-            // [Edit configuration here ...]
-            //MARK: General settings
-            config.albumName = "LittlePink"
-            config.screens = [.library]
-            
-            //MARK: Library settings
-            config.library.mediaType = .photo
-            config.library.defaultMultipleSelection = true
-            config.library.maxNumberOfItems = kMaxPhotoCount - photoCount
-            config.library.preSelectItemOnMultipleSelection = false
-            
-            //MARK: Gallery settings
-            config.gallery.hidesRemoveButton = false
-            
 
-            // Build a picker with your configuration
-            let picker = YPImagePicker(configuration: config)
-
-            picker.didFinishPicking { [unowned picker] items, _ in
-                
-                for item in items{
-                    if case let .photo(photo) = item{
-                        self.photos.append(photo.image)
-                    }
-                }
-                self.photoCollectionView.reloadData()
-                
-                picker.dismiss(animated: true)
-            }
-            present(picker, animated: true)
-        }else{
-            showTexHUD("Up to \(kMaxPhotoCount) photos can be selected")
-        }
-    }
 }
+
