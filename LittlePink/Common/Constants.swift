a032b09c7b07ada267db858b07d866545f051d2d
YuchenJiaa <113528470+YuchenJiaa@users.noreply.github.com>
2023-03-27 20:23:45 -0400

    Add gesture and jump to another page

--- LittlePink/Common/Constants.swift
+++ LittlePink/Common/Constants.swift
@@ -22,8 +22,24 @@ let kWaterfallCellID = "WaterfallCellID"
 let kWaterfallPadding: CGFloat = 4
 
 let kChannels = ["Recommendation","Travel","Entertainment","Skill","Beauty","Fitness","Food","Pet"]
+
+//YPImagePicker
 let kMaxPhotoCount = 9
 let kMaxCameraZoomFactor: CGFloat = 5
 
+//Note
 let kMaxNoteTitleCount = 50
 let kMaxNoteTextCount = 2000
+
+//Topic
+let kSubchannels = [
+    ["Fashion and beauty","Food and cooking","Travel and tourism","Lifestyle and inspiration"],
+    ["Solo traveling","Top travel destinations","Best beaches"],
+    ["Celebrity gossip and news","Movie and TV show reviews","Music news and reviews"],
+    ["DIY and home improvement projects","Language learning and tutoring"],
+    ["Makeup tutorials and tips","Skincare routines and product reviews","Haircare tips and tutorials"],
+    ["Workout routines and tutorials","Fitness challenges and competitions"],
+    ["Recipe ideas and cooking tutorials","Healthy eating and nutrition advice","Food photography and styling tips","International cuisine and cultural foods"],
+    ["Cute animal photos and videos","Pet care and health tips","Pet adoption"],
+
+]
